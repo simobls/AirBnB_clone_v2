fce721d70cb0c7cd5021d9bf34408047491dc95c
simobls <belassalsimo78@gmail.com>
2024-04-19 16:55:33 +0100

    add

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,19 +1,22 @@
-#!/home/shadowwwbls/AirBnB_clone_v2/web_flask/env/bin/python
+#!/usr/bin/python3
+"""
+start Flask application
+"""
 
 from flask import Flask
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    return "Hello HBNB!"
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    return "HBNB"
-
+    """returns HBNB"""
+    return 'HBNB'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
